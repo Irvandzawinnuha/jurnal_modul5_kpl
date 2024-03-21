c18f6171832691c1a314a8b2e22fffe208dc62a1
irvandzawinnuha <irvandzwinnuha15@gmail.com>
2024-03-21 10:40:59 +0700

    fisrt commit

---
+++ jurnalmod5_1302223076/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine(Penjumlahan.JumlahTigaAngka<int>(13, 02, 22));
+    }
+}
+
+public class Penjumlahan 
+ {
+ 
+    public static T JumlahTigaAngka<T> (T Data1, T Data2, T Data3)
+    {
+        dynamic angka1 = Data1;
+        dynamic angka2 = Data2;
+        dynamic angka3 = Data3;
+        dynamic hasil = angka1 + angka2 + angka3;
+
+        return hasil;
+       
+    }
+
+   
+    
+
+}
+
